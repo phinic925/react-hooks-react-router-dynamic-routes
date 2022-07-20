4fb06b0db2e4f037367124e5bfdd78f93f4a7e19
phinic925 <phinicnyangate480@gmail.com>
2022-07-20 16:10:58 +0100

    Done

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -3,9 +3,16 @@ import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route path={`${match.url}/:movieId`}>
+      <h3>Choose a movie from the list above</h3>
+        {/* adding the movies object as a prop to MovieShow */}
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
 }
